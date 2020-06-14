e4f2a155f8ba3d160e2275847c5808f9b469f51b
MightyCoderX <mattiaca003@gmail.com>
2020-06-14 19:33:04 +0200

    Updated

--- js/index.js
+++ js/index.js
@@ -1,36 +1,7 @@
-const navbar = document.querySelector('.navbar');
-const navbarDefaultTransition = navbar.style.transition;
-const menuIcon = document.querySelector('.menu-icon');
-
-menuIcon.addEventListener('click', e =>
+document.body.addEventListener("wheel", event =>
 {
-    navbar.querySelector('.nav-links').classList.toggle('shown');
-});
-
-document.querySelector('.logo').onclick = e=> open('/', '_self');
-
-window.addEventListener('scroll', e =>
-{
-    navbar.querySelector('.nav-links').classList.remove('shown');
-
-    let navAppearOffsetY = navbar.clientHeight * 1.7;
-
-    if(scrollY >= navbar.clientHeight && scrollY < innerHeight - navAppearOffsetY)
-    {
-        navbar.style.position = "fixed";
-        navbar.style.top = `-${navbar.clientHeight}px`;
-    }
-    else if(scrollY >= innerHeight - navAppearOffsetY)
-    {
-        navbar.style.top = 0;
-        navbar.style.transition = navbarDefaultTransition;
-        navbar.classList.add('top-enter');
-    }
-    else
-    {
-        navbar.style.position = "absolute";
-        navbar.style.transition = "0s";
-        navbar.classList.remove('top-enter');
-        navbar.style.top = 0;
-    }
-});
+  if (event.deltaY > 0)
+    document.body.scrollBy(0, scrollY+30);
+  else
+  document.body.scrollBy(0, scrollY-30);;
+}, false);
