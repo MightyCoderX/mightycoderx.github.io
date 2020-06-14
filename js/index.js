6fe5a67572b3628b4b795f21a8da0cf675c52640
MightyCoderX <mattiaca003@gmail.com>
2020-06-15 01:11:23 +0200

    Updated

--- js/index.js
+++
@@ -1,7 +0,0 @@
-document.body.addEventListener("wheel", event =>
-{
-  if (event.deltaY > 0)
-    document.body.scrollBy(0, scrollY+30);
-  else
-  document.body.scrollBy(0, scrollY-30);;
-}, false);
